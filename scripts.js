d89868f5bd7d9651b73ff9f714ba7b8fa3690bdf
terterslk93 <t.thevasurendran@ileps.fr>
2024-12-15 19:36:51 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        document.querySelector(e.target.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
